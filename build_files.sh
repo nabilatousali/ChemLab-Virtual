59ebb8215fd88dfbd8c6318d1bbe82b826419e19
nabilatousali <nabilatousali53@gmail.com>
2026-09-21 21:48:48 +0100

    Corriger pip dans build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 set -e
 python3 --version
-python3 -m pip install -r requirements.txt
+python3 -m pip install --break-system-packages -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
